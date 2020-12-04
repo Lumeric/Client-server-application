2bfae1b85d8079f39802f60075cf1fa541926b6f
Lumeric <71871188+Lumeric@users.noreply.github.com>
2020-12-04 18:09:55 +0700

    Ip validation

--- Client-server application/Client/ViewModels/LoginViewModel.cs
+++ Client-server application/Client/ViewModels/LoginViewModel.cs
@@ -16,55 +16,98 @@
         {
             _loginController = loginController ?? throw new ArgumentNullException(nameof(loginController));
 
-            LoginCommand = new DelegateCommand(ExecuteLoginCommand);
+            //LoginCommand = new DelegateCommand(ExecuteLoginCommand);
 
-            ValidUsername = new DelegateCommand(ValidatingUsername);
+            //ValidUsername = new DelegateCommand(ExecuteValidateUsername);
+
+            Validation = new DelegateCommand(ExecuteValidation);
         }
 
-        public DelegateCommand LoginCommand { get; }
-        public DelegateCommand ValidUsername { get; }
+        //public DelegateCommand LoginCommand { get; }
+        //public DelegateCommand ValidUsername { get; }
+        public DelegateCommand Validation { get; }
 
         private void ExecuteLoginCommand()
         {
             _loginController.LoginUser();
         }
-        
 
-        private readonly string regexUsername = @"\w";
-        private string _username;
+        private readonly string regexIP = @"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+        private readonly string regexUsername = @"[a-z0-9](?:[-_\.]?[a-z0-9])*";
+        private string _username = "";
+        private string _ip = "";
         public string Username
         {
             get => _username;
             set => SetProperty(ref _username, value);
         }
-
-        public bool ValidateUsername(string username, out string errorMessage)
+        public string IP
         {
+            get => _ip;
+            set => SetProperty(ref _ip, value);
+        }
+
+        private void ValidateUsername(string username)
+        {
+            string errorMessage;
+
             if (username.Length == 0)
             {
                 errorMessage = "Username is required.";
-                return false;
             }
-
-            if (Regex.IsMatch(username, regexUsername))
+            else if (!Regex.IsMatch(username, regexUsername))
             {
-                errorMessage = "";
-                return true;
+                errorMessage = "Username must be valid username format and contains only alphabetic symbols and numbers.\n" +
+                               "For example 'Cyberpunk2020'";  
+            }
+            else if (username.Length < 6 || username.Length > 20)
+            {
+                errorMessage = "Username length nust be no less than 6 symbols and no more than 20 symbols.";
+            }
+            else
+            {
+                errorMessage = "OK";
             }
 
-            errorMessage = "Username must be valid username  format.\n" +
-               "For example 'Hasagi' ";
-            return false;
+            Username = errorMessage;
         }
 
-        public void ValidatingUsername()
-        {
-            string errorMsg;
+        //private void ExecuteValidateUsername()
+        //{
+        //    //_loginController.ValidateUser();
+        //    ValidateUsername(Username);
+        //}
 
-            if (ValidateUsername(_username, out errorMsg))
+        private void ValidateIP(string ip)
+        {
+            string errorMessag;
+
+            if (ip.Length == 0)
             {
-                Username = "Dummy";
+                errorMessag = "IP is required.";
             }
+            else if (!Regex.IsMatch(ip, regexIP))
+            {
+                errorMessag = "IP must be valid ip format.\n" +
+                               "For example '192.168.1.0'";
+            }
+            else
+            {
+                errorMessag = "OK";
+            }
+
+            IP = errorMessag;
+        }
+
+        //необходимо объединить методы валидации. Также создать один метод который будем вызывать все методы валидации,
+        //и биндить его к view. Также подумать над тем, чтобы перенести валидацию в бизнес логику.
+        //также подумать над конструкцией if/else, так как по идее можно сделать лучше.
+
+        //общий метод вызова валидации
+        private void ExecuteValidation()
+        {
+            //ValidateUsername(Username);
+            ValidateIP(IP);
         }
     }
 }
