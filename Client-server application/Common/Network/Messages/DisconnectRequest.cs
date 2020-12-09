ac2f90ff00624d671c915b07346573308288fefd
Lumeric <71871188+Lumeric@users.noreply.github.com>
2020-12-09 18:22:02 +0700

    WsClient changes

--- Client-server application/Common/Network/Messages/DisconnectRequest.cs
+++ Client-server application/Common/Network/Messages/DisconnectRequest.cs
@@ -8,6 +8,35 @@ namespace Common.Network.Messages
 {
     public class DisconnectRequest
     {
+        #region Properties
 
+        public string Username { get; }
+
+        #endregion //Properties
+
+
+        #region Constructors
+
+        public DisconnectRequest(string username)
+        {
+            Username = username;
+        }
+
+        #endregion //Constructors
+
+        #region Methods
+
+        public MessageContainer GetContainer()
+        {
+            var container = new MessageContainer
+            {
+                Identifier = nameof(DisconnectRequest),
+                Payload = this
+            };
+
+            return container;
+        }
+
+        #endregion //Methods
     }
 }
--- Client-server application/Common/Network/WebSocket/WsClient.cs
+++ Client-server application/Common/Network/WebSocket/WsClient.cs
@@ -1,33 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Newtonsoft.Json;
-
-using WebSocketSharp;
-using WebSocketSharp.Server;
-
-
-
-namespace Common.Network
+﻿namespace Common.Network
 {
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Messages;
+    using Newtonsoft.Json;
+
+    using WebSocketSharp;
+    using WebSocketSharp.Server;
+
     public class WsClient : ITransport
     {
         #region Fields
+
+        private readonly ConcurrentQueue<MessageContainer> _sendQueue;
+
+        private WebSocket _socket;
+
+        private int _sending;
+        private string _login;
+
         #endregion Fields
 
         #region Properties
+        public bool IsConnected => _socket?.ReadyState == WebSocketState.Open;
+
         #endregion Properties
 
         #region Events
 
         public event EventHandler<MessageReceivedEventArgs> MessageReceived;
+        public event EventHandler<UserDisconnectedEventArgs> UserDisconnected;
+
 
         #endregion Events
 
         #region Constructors
+
+        public WsClient()
+        {
+            _sendQueue = new ConcurrentQueue<MessageContainer>();
+            _sending = 0;
+        }
+
         #endregion Constructors
 
         #region Methods
@@ -47,9 +66,53 @@ namespace Common.Network
             throw new NotImplementedException();
         }
 
-        public void Send(string message)
+        public void Send(List<Guid> listClientId, MessageContainer message)
         {
-            throw new NotImplementedException();
+            _sendQueue.Enqueue(message);
+
+            if (Interlocked.CompareExchange(ref _sending, 1, 0) == 0)
+                SendImpl();
+        }
+
+        private void SendCompleted(bool completed)
+        {
+            // При отправке произошла ошибка.
+            if (!completed)
+            {
+                Disconnect();
+                UserDisconnected?.Invoke(this, new UserDisconnectedEventArgs(_login));
+                return;
+            }
+
+            SendImpl();
+        }
+
+        private void SendImpl()
+        {
+            if (!IsConnected)
+                return;
+
+            if (!_sendQueue.TryDequeue(out var message) && Interlocked.CompareExchange(ref _sending, 0, 1) == 1)
+                return;
+
+            var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+            string serializedMessages = JsonConvert.SerializeObject(message, settings);
+            _socket.SendAsync(serializedMessages, SendCompleted);
+        }
+
+        private void OnMessage(object sender, MessageEventArgs e)
+        {
+
+        }
+
+        private void OnOpen(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void OnClose(Object sender, CloseEventArgs e)
+        {
+
         }
 
         #endregion Methods
